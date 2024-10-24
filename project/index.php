182238e049f6c470918144faa0b374d135efa0a8
artemihx <art.pozx1@gmail.com>
2024-10-24 21:48:55 +0700

    Tasks

--- project/index.php
+++ project/index.php
@@ -1,1 +1,53 @@
-<?php phpinfo()?>
+<?php
+
+use Models\Articles\Article;
+use Models\Users\User;
+
+function myAutoLoader(string $classname)
+{
+    $classname = str_replace('Project\\', '', $classname);
+    $filePath = __DIR__ . '/' . str_replace('\\', '/', $classname) . '.php';
+    require_once $filePath;
+}
+
+spl_autoload_register('myAutoLoader');
+
+/*
+$controller = new \Controllers\MainController();
+
+if(!empty($_GET['name']))
+{
+    $controller->sayHello($_GET['name']);
+} else {
+    $controller->main();
+}
+*/
+
+$route = $_GET['route'] ?? '';
+$routes = require __DIR__ . '/Routes.php';
+$route = ltrim($route, '/');
+
+$isRouteFound = false;
+foreach ($routes as $pattern => $controllerAndAction) {
+    preg_match($pattern, $route, $matches);
+    if(!empty($matches))
+    {
+        if(!empty($matches))
+        {
+            $isRouteFound = true;
+            break;
+        }
+    }
+}
+
+if(!$isRouteFound)
+{
+    echo 'Страница не найдена!';
+    return;
+}
+$controllerName = $controllerAndAction[0];
+$actionName = $controllerAndAction[1];
+
+$controller = new $controllerName();
+$controller->$actionName(...$matches);
+?>
